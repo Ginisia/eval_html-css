4f81f59516c9483146a7d2e7b07c7a2eb3978191
unknown <djinymail@yahoo.fr>
2022-02-09 15:41:44 +0100

    Last Commit

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,5 @@
 var burgermen = document.querySelector('.burger-menu');
 var hiddenmenu = document.querySelector('.window-coucou');
-var cross = document.querySelector('.cross-wind');
 
 burgermen.addEventListener('click', function () {
 	hiddenmenu.classList.toggle('active');
