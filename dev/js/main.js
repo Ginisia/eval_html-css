99cd0d11ee4a90f727e9444cd974707fb7d12dfe
unknown <djinymail@yahoo.fr>
2022-02-09 11:59:56 +0100

    Starting responsive, burger done

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,7 @@
+var burgermen = document.querySelector('.burger-menu');
+var hiddenmenu = document.querySelector('.window-coucou');
+var cross = document.querySelector('.cross-wind');
 
+burgermen.addEventListener('click', function () {
+	hiddenmenu.classList.toggle('active');
+});
